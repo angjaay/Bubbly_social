b32ef5e9045a290b47eab8f116c561c6d179f994
angjaay <angga.angzai@gmail.com>
2022-04-10 11:24:59 +0700

    create spinner

---
+++ bubbly_frontend/src/components/Spinner.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {Grid} from 'react-loader-spinner';
+
+
+function Spinner({ message }) {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <Grid
+        type="Circles"
+        color="#3b82f6"
+        height={50}
+        width={200}
+        className="m-5"
+      />
+
+      <p className="text-lg text-center px-2">{message}</p>
+    </div>
+  );
+}
+
+export default Spinner;
