f9a54491a55611c22af06c10308e2b6b831e5eda
angjaay <angga.angzai@gmail.com>
2022-04-11 11:16:20 +0700

    config

--- bubbly_frontend/src/App.js
+++ bubbly_frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './container/Home';
 import Login from './components/Login';
+import { fetchUser } from './utils/fetchUser';
 
 const App = () => {
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
+
+     
+
   return (
     <Routes>
         <Route path="login" element={<Login />} />
